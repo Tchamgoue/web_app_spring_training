8c5de360dd1a6144b7ef445afd7f40bcf0f1e922
Adrienne Tchamgoue <adrienne.tchamgoue@gmail.com>
2022-10-26 16:52:26 +0200

    Partie 4 - Filtre pour ajouter un header HTTP

---
+++ src/main/java/todo/ApplicationIdentifierFilter.java
@@ -0,0 +1,37 @@
+package todo;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+
+
+@Component
+public class ApplicationIdentifierFilter implements javax.servlet.Filter{
+
+
+    private final  String instanceId = UUID.randomUUID().toString();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        // before the rest (filters, servlet)
+
+
+        if (servletResponse instanceof HttpServletResponse httpResponse){
+            //((HttpServletResponse) servletResponse).addHeader();
+            httpResponse.setHeader("Instance-Id", instanceId);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        // after the rest (filters, servlet)
+        //httpResponse.setHeader("titi", "toto");
+    }
+
+
+}
---
+++ src/main/java/todo/Student.java
@@ -0,0 +1,10 @@
+package todo;
+
+import java.time.YearMonth;
+
+public class Student {
+
+    public String firsName;
+    public String lastName;
+    public YearMonth birthYearMonth;
+}
